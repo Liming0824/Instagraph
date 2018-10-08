d51e967018c0df1226fe41c7dff0b647809b8efb
Liming Kang <lkang9208@yahoo.com>
2018-10-08 17:59:48 -0400

    comment out future feature

--- frontend/components/posts/post_item.jsx
+++ frontend/components/posts/post_item.jsx
@@ -119,8 +119,12 @@ class PostItem extends React.Component {
           <div className="comment-icons">
             <a className="comment-like" onClick={this.handleLike.bind(this)}><img src={this.props.liked ? window.redlikeImg : window.likeImg}/></a>
             <a className="comment-write" onClick={this.handleCommentImg.bind(this, this.props.post)}><img src={window.commentImg}/></a>
-            // <a className="comment-share" onClick={this.handleShare.bind(this)}><img src={window.shareImg}/></a>
-            // <a className="comment-flag" onClick={this.handleTag.bind(this)}><img src={window.tagImg}/></a>
+
+
+
+
+
+
           </div>
           <div className="likes-count">{this.props.post.likes.length} likes</div>
           <div className="comment-content">
@@ -167,3 +171,6 @@ const mapDispatchToProps = (dispatch) => {
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(PostItem));
 //here photo_image_url is in aws, if use 'image_url' then should be database local image
+// <a className="comment-share" onClick={this.handleShare.bind(this)}><img src={window.shareImg}/></a>
+// <a className="comment-flag" onClick={this.handleTag.bind(this)}><img src={window.tagImg}/></a>
+// should be added in <ul classname='comment' />
