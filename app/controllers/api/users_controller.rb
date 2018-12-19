2f8adaeb7f0957d1db45c1a93e972cf5e527dc8a
Liming Kang <lkang9208@yahoo.com>
2018-12-18 19:02:21 -0500

    deliver_now

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,7 +10,8 @@ class Api::UsersController < ApplicationController
     @user = User.create(user_params)
     if @user.save
       login(@user)
-      UserMailer.welcome_email(@user).deliver
+      UserMailer.welcome_email(@user).deliver_now
+      # UserMailer.with(user: @user).welcome_email.deliver_now
       render 'api/users/show'
     else
       render json: @user.errors.full_messages, status: 422
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :amazon_dev
 
