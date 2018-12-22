ce2ad789c0093306a6f247f3c83d34c689457c49
Liming Kang <lkang9208@yahoo.com>
2018-12-22 18:20:11 -0500

    manage email format

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     # @user = params[:user]
-    @url  = 'http://example.com/login'
+    @url  = 'https://best-instagraph.herokuapp.com'
     mail(to: 'lkang9208@yahoo.com', subject: 'Welcome to My Awesome Site')
     # mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
